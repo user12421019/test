8c32e1e7d5dbbc3e9b8704565e91906cf3e5bc1c
user12421019 <yuki.kamata1001@proton.me>
2025-08-28 12:03:31 +0900

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var p = document.getElementById('aaaaa');
 var children = p.childNodes;
-var textnode = children.item(0); 
-console.log(textnode.nodeValue);
-textnode.nodeValue = "書き換えました";
+
+while (p.hasChildNodes()) {
+  p.removeChild(children.item(0));
+}
