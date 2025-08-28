c0426f97b02cae910b5cd3ef5e82983207b7b6b8
user12421019 <yuki.kamata1001@proton.me>
2025-08-28 12:16:53 +0900

    update

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var table = document.getElementById('aaaaa');
 
-table.deleteRow(0);
+var tbody = table.tBodies.item(0);
+
+var newtr = tbody.insertRow(0);
+
+for (var i = 0; i < 2; i++) {
+  var newtd = newtr.insertCell(newtr.cells.length);
+  newtd.appendChild(document.createTextNode('testtest' + i));
+}
