d600923679499ffbbfcf7ee24dd18645f4ee6a23
user12421019 <yuki.kamata1001@proton.me>
2025-08-28 12:18:31 +0900

    text

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-var table = document.getElementById('aaaaa');
-
-var tbody = table.tBodies.item(0);
-
-var newtr = tbody.insertRow(0);
-
-for (var i = 0; i < 2; i++) {
-  var newtd = newtr.insertCell(newtr.cells.length);
-  newtd.appendChild(document.createTextNode('testtest' + i));
-}
+var title = document.getElementsByTagName('title').item(0);
+console.log(title.text);
